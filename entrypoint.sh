005b397e44581c7ec79dd528707aa873032f9b3f
Brian Bockelman <bbockelm@cse.unl.edu>
2019-07-16 12:26:55 -0500

    Automatically set the X509_USER_PROXY env var after creating the proxy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+if [ -e /etc/grid-security/hostkey.pem ]; then
+
+  grid-proxy-init \
+    -cert /etc/grid-security/hostcert.pem \
+    -key /etc/grid-security/hostkey.pem \
+    -hours 168 \
+    -out /tmp/x509_pilot_proxy || exit 1
+
+  export X509_USER_PROXY=/tmp/x509_pilot_proxy
+
+fi
+
 cd /tmp
 export _CONDOR_SCRATCH_DIR=$PWD
 exec syracuse_glidein_startup.sh
