c7fd8956cda454ed9491263ea16dc68a1ecb3239
Brian Bockelman <bbockelm@cse.unl.edu>
2019-07-16 13:39:55 -0500

    Make entrypoint more robust against UID issues.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,18 @@
 
 if [ -e /etc/grid-security/hostkey.pem ]; then
 
+  cp /etc/grid-security/hostcert.pem /tmp/hostcert.pem
+  chmod 0644 /tmp/hostcert.pem
+  cp /etc/grid-security/hostkey.pem /tmp/hostkey.pem
+  chmod 0600 /tmp/hostkey.pem
+
   grid-proxy-init \
-    -cert /etc/grid-security/hostcert.pem \
-    -key /etc/grid-security/hostkey.pem \
+    -cert /tmp/hostcert.pem \
+    -key /tmp/hostkey.pem \
     -hours 168 \
     -out /tmp/x509_pilot_proxy || exit 1
+
+  rm -f /tmp/hostcert.pem /tmp/hostkey.pem
 
   export X509_USER_PROXY=/tmp/x509_pilot_proxy
 
